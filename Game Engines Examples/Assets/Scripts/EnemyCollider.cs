c1c7fe0cd91372913f1e5dfc2e9344912f00087a
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-10-16 18:33:52 +0100

    Week 4 lab completed

---
+++ Game Engines Examples/Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+
+    private Collider m_Collider;
+    public float destroyVelocityStart = -10f;
+    public float destroyVelocityEnd = 0f;
+
+    void OnCollisionEnter(Collision c)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.drag = 1;
+
+        Debug.Log("Collison: " +  c.gameObject.tag);
+        if (c.gameObject.tag == "PlayerBullet")
+        {
+            m_Collider.enabled = false;
+            foreach (Transform child in transform) {
+                Collider child_Collider = child.GetComponent<Collider>();
+
+                Rigidbody child_rb = child.gameObject.AddComponent<Rigidbody>();
+
+                child_rb.useGravity = true;
+                child_rb.isKinematic = false;
+
+                float x = Random.Range(destroyVelocityStart, destroyVelocityEnd);
+                float y = Random.Range(destroyVelocityStart, destroyVelocityEnd);
+                float z = Random.Range(destroyVelocityStart, destroyVelocityEnd);
+
+                child_rb.angularVelocity = new Vector3(x, y, z);
+                
+                child_rb.velocity = new Vector3(x, Math.Abs(y), z);
+
+                child_Collider.enabled = true;
+            }
+
+            Destroy(this.gameObject, 7);
+        }
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        //Debug.Log("Collision Stay");
+    }
+
+    void OnCollisionExit(Collision c)
+    {
+        //Debug.Log("Collision Exit");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Engines Examples/Assets/Scripts/Spawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    public float radius = 10;
+
+    public int spawnRate = 5;
+
+    public int max = 10;
+
+    public GameObject spawnObject;
+
+    void Spawn()
+    {
+        GameObject cube = GameObject.Instantiate<GameObject>(spawnObject);
+        // cube.GetComponent<Renderer>().material.color = 
+        //     Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+        Vector3 pos = new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius));
+
+        cube.AddComponent<Rigidbody>();
+        cube.transform.position = transform.TransformPoint(pos);
+
+        cube.transform.parent = this.transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("Spawn", 5);
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    int count = 0;
+
+    System.Collections.IEnumerator SpawnCoroutine()
+    {
+        while(true)
+        {
+            
+            GameObject[] cubes = GameObject.FindGameObjectsWithTag("BlueTank");
+            print(cubes.Length);
+            if (cubes.Length < max)
+            {
+                Spawn();
+            }
+            yield return new WaitForSeconds(1.0f / (float)spawnRate); 
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
